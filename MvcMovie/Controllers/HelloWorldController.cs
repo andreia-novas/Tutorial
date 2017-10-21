05ef3adf6ff79a2f8071ffa8f2c342da68d02c26
andreia-novas <andreianovas93@gmail.com>
2017-10-21 17:58:18 +0100

    Modificado o codigo do HelloWorldController

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,19 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
         }
     }
 }
